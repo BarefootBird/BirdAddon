c8cb46f1fc04e8c2a88c3f14748d3f1e9256afdb
Bird <barefootbird644@gmail.com>
2026-05-02 12:52:19 +1200

    Revert "1.21.10 vers"
    This reverts commit acb24e4fce25dc11d7a4a850da94f8c5566148e2.

--- src/main/kotlin/com/barefootbird/birdaddon/utils/ReplayUtils.kt
+++ src/main/kotlin/com/barefootbird/birdaddon/utils/ReplayUtils.kt
@@ -261,7 +261,7 @@ class LogSelectScreen(
 
     override fun init() {
         super.init()
-        list = LogListWidget(minecraft!!, width, 32, height - 150, 20)
+        list = LogListWidget(minecraft, width, 32, height - 150, 20)
         addRenderableWidget(list)
 
         addRenderableWidget(
@@ -269,7 +269,7 @@ class LogSelectScreen(
                 val selected = list.selected
                 if (selected != null) {
                     onReplaySelected(selected.file)
-                    minecraft!!.setScreen(null) // close the screen
+                    minecraft.setScreen(null) // close the screen
                 }
             }.bounds(width / 2 - 100, height - 70, 200, 20).build()
         )
@@ -295,7 +295,7 @@ class LogSelectScreen(
 
                     if (oldFile.renameTo(newFile)) {
                         removeWidget(list)
-                        list = LogListWidget(minecraft!!, width, 32, height - 150, 20)
+                        list = LogListWidget(minecraft, width, 32, height - 150, 20)
                         addRenderableWidget(list)
                         //list.children().remove(selected)
                         //list.addNewEntry(LogEntry(newFile))
