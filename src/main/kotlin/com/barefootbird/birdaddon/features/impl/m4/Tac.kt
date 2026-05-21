fe63b7e3ceb647f39ada5816988eda260626ebc1
Bird <barefootbird644@gmail.com>
2026-05-22 01:03:01 +1200

    fix tac timer being on when not enabled

--- src/main/kotlin/com/barefootbird/birdaddon/features/impl/m4/Tac.kt
+++ src/main/kotlin/com/barefootbird/birdaddon/features/impl/m4/Tac.kt
@@ -1,38 +1,21 @@
 package com.barefootbird.birdaddon.features.impl.m4
 
 import com.barefootbird.birdaddon.utils.Category
-import com.barefootbird.birdaddon.utils.M4State
-import com.barefootbird.birdaddon.utils.M4State.bearKillRegex
-import com.barefootbird.birdaddon.utils.M4State.bearKillTimes
-import com.barefootbird.birdaddon.utils.M4State.bearSpawnRegex
 import com.barefootbird.birdaddon.utils.M4State.bearSpawnStartTimes
-import com.barefootbird.birdaddon.utils.M4State.bearSpawnTimes
 import com.barefootbird.birdaddon.utils.M4State.bearTimer
-import com.barefootbird.birdaddon.utils.M4State.endRegex
-import com.barefootbird.birdaddon.utils.M4State.ended
-import com.barefootbird.birdaddon.utils.M4State.enteredRegex
-import com.barefootbird.birdaddon.utils.M4State.inThornBoss
-import com.barefootbird.birdaddon.utils.M4State.timer
-import com.barefootbird.birdaddon.utils.M4State.updateKills
 import com.odtheking.odin.clickgui.settings.Setting.Companion.withDependency
 import com.odtheking.odin.clickgui.settings.impl.BooleanSetting
 import com.odtheking.odin.clickgui.settings.impl.StringSetting
-import com.odtheking.odin.events.ChatPacketEvent
 import com.odtheking.odin.events.TickEvent
 import com.odtheking.odin.events.WorldEvent
 import com.odtheking.odin.events.core.on
 import com.odtheking.odin.events.core.onSend
 import com.odtheking.odin.features.Module
 import com.odtheking.odin.utils.Colors
-import com.odtheking.odin.utils.modMessage
 import com.odtheking.odin.utils.render.textDim
 import com.odtheking.odin.utils.skyblock.dungeon.DungeonClass
 import com.odtheking.odin.utils.skyblock.dungeon.DungeonUtils
 import com.odtheking.odin.utils.toFixed
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import net.minecraft.network.protocol.game.ServerboundInteractPacket
 import net.minecraft.network.protocol.game.ServerboundUseItemPacket
 import net.minecraft.world.InteractionHand
 
@@ -70,7 +53,9 @@ object Tac: Module(
         onSend<ServerboundUseItemPacket> { event ->
             val item = mc.player?.getItemInHand(InteractionHand.MAIN_HAND)
             if (item?.displayName?.string?.lowercase()?.contains("tactical insertion") == true) {
-                tacTime = 60
+                if (tacTimer) {
+                    tacTime = 60
+                }
                 if (printTacTime && bearTimer != -1) {
                     if (DungeonUtils.inBoss && DungeonUtils.isFloor(4)) {
                         if (DungeonUtils.currentDungeonPlayer.clazz == DungeonClass.Tank || !printTimeOnlyOnTank) {
