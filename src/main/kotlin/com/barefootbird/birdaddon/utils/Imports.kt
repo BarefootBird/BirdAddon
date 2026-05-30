0591e5dc964ddb6e178e5ec37760232844f1f268
Bird <barefootbird644@gmail.com>
2026-05-30 15:17:03 +1200

    fix capitalization

--- src/main/kotlin/com/barefootbird/birdaddon/utils/Imports.kt
+++ src/main/kotlin/com/barefootbird/birdaddon/utils/Imports.kt
@@ -6,8 +6,8 @@ package com.barefootbird.birdaddon.utils
 // typealias Chicken = net.minecraft.world.entity.animal.chicken.Chicken
 // typealias Cow = net.minecraft.world.entity.animal.cow.Cow
 // typealias Rabbit =  net.minecraft.world.entity.animal.rabbit.Rabbit
-// typealias skeleton = net.minecraft.world.entity.monster.skeleton.Skeleton
-// typealias witherSkeleton = net.minecraft.world.entity.monster.skeleton.WitherSkeleton
+// typealias Skeleton = net.minecraft.world.entity.monster.skeleton.Skeleton
+// typealias WitherSkeleton = net.minecraft.world.entity.monster.skeleton.WitherSkeleton
 // typealias Zombie = net.minecraft.world.entity.monster.zombie.Zombie
 
 // 1.21.10
