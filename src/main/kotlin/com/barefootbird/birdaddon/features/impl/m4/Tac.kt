a158499df92ffc053e7f4f8688cd8e89ed16912b
Bird <barefootbird644@gmail.com>
2026-06-09 00:35:40 +1200

    add option to add to party chat for tac

--- src/main/kotlin/com/barefootbird/birdaddon/features/impl/m4/Tac.kt
+++ src/main/kotlin/com/barefootbird/birdaddon/features/impl/m4/Tac.kt
@@ -4,6 +4,7 @@ import com.barefootbird.birdaddon.events.M4Event
 import com.barefootbird.birdaddon.utils.Category
 import com.barefootbird.birdaddon.utils.M4State
 import com.barefootbird.birdaddon.utils.modMessage
+import com.barefootbird.birdaddon.utils.sendCommand
 import com.odtheking.odin.clickgui.settings.Setting.Companion.withDependency
 import com.odtheking.odin.clickgui.settings.impl.BooleanSetting
 import com.odtheking.odin.clickgui.settings.impl.StringSetting
@@ -38,8 +39,9 @@ object Tac: Module(
     private val outsideOfM4 by BooleanSetting("Show Tac Timer out of m4", false, "Shows tac timer outside of m4").withDependency { tacTimer }
     private val prefix by StringSetting("Tac Timer Prefix", "§6Tac: ", desc="Prefix for the tac timer").withDependency { tacTimer }
 
-    val printTacTime by BooleanSetting("Print Last Bear Tac Time", true, "Print tac time")
-    private val printTimeOnlyOnTank by BooleanSetting("Print Time Only On Tank", true, "Prints the last bear tac time only when on tank")
+    private val printTacTime by BooleanSetting("Print Tac Time", true, "Print tac time")
+    private val printTimeOnlyOnTank by BooleanSetting("Print Time Only On Tank", true, "Prints the tac time only when on tank")
+    private val printTacToPartyChat by BooleanSetting("Print Tac to pchat", true, "Prints tac time to party chat")
 
     private fun timerText (example: Boolean): String {
         if (example) return "${prefix}1.55s"
@@ -51,8 +53,6 @@ object Tac: Module(
     }
 
     var tacTime = -1
-    var lastBearTacTime = -1
-    var lastBearTaccedOn = -1
 
     init {
 
@@ -65,8 +65,13 @@ object Tac: Module(
                 if (printTacTime && M4State.bearTimer != -1) {
                     if (DungeonUtils.inBoss && DungeonUtils.isFloor(4)) {
                         if (DungeonUtils.currentDungeonPlayer.clazz == DungeonClass.Tank || !printTimeOnlyOnTank) {
-                            lastBearTacTime = M4State.bearTimer
-                            lastBearTaccedOn = M4State.bearSpawnStartTimes.size
+                            if (printTacToPartyChat) {
+                                sendCommand("pc BirdAddon > Tacced at ${((M4State.bearTimer / 20.0).toFixed(2))}s")
+                            } else {
+                                modMessage(
+                                    "Tacced at ${((M4State.bearTimer / 20.0).toFixed(2))}s"
+                                )
+                            }
                         }
                     }
                 }
@@ -77,22 +82,6 @@ object Tac: Module(
             if (tacTime >= 0) {
                 tacTime--
             }
-        }
-
-        on<M4Event.End> {
-            if (printTacTime && lastBearTaccedOn == M4State.bearKillTimes.size) {
-                GlobalScope.launch {
-                    delay(1000)
-                    modMessage(
-                        "Tacced at ${((lastBearTacTime / 20.0).toFixed(2))}s"
-                    )
-                }
-            }
-        }
-
-        on<WorldEvent.Load> {
-            lastBearTacTime = -1
-            lastBearTaccedOn = -1
         }
     }
 }
